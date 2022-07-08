2c8b3c4c158d94625b50667e3ed5d2bc583ad5cc
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-08 22:09:11 +0300

    added task_5

---
+++ Task_5/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Write a number N: ");
+int n = int.Parse(Console.ReadLine());
+
+int i = 0;
+
+if (n < 0)
+{
+     i = n;
+    n = -n;
+}
+else
+{
+   i = -n;
+}
+
+while (i <= n)
+{
+    Console.Write($"{i}");
+    i++;
+}
