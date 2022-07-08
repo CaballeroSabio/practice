d0b063f0675961b6109232e89e21dd5200e6c3f2
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-08 21:18:56 +0300

    added Task_0 1

--- Task_0/Program.cs
+++ Task_0/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0. Демонстрация решения
+// Напишите программу,
+// 1. которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Clear();
+string str = "Hi!";
+Console.Write("Enter an integer: ");
+int num=Convert.ToInt32(Console.ReadLine()); // int num=4; // -2.147 млрд до 2.147 млрд
+// double d=4.6;
+int res=num*num;
+Console.WriteLine($"Квадрат числа {num} = {res}");
