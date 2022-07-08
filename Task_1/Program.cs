54f3407c422b1683328ae6aa42cef96356a66cec
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-08 21:43:39 +0300

    added Task_1.2

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Clear();
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int res = num2*num2;
+if (num1==res)
+{
+    Console.Write("Первое число не является квадратом второго");
+}
+else
+{
+    Console.Write("Первое число не является квадратом второго");
+}
