ede106a96d5f3421ffbb84bf769c7d1ae929b0a5
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-08 22:19:33 +0300

    added task_7

---
+++ Task_7/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int num = 4567789   ; // 45.6
+int lastDigit = num % 10;
+Console.WriteLine(lastDigit);
