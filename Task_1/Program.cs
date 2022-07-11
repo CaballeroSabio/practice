86aaadde211fb933530eccbdcb8cc64e85c4acc1
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-11 13:23:15 +0300

    fixed line 11

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,15 +2,16 @@
 
 Console.Clear();
 Console.Write("Введите первое число: ");
-int day_week = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 int res = num2*num2;
-if (num1==res)
+if (num1 == res)
 {
-    Console.Write("Первое число не является квадратом второго");
+    Console.Write("Первое число является квадратом второго");
 }
 else
 {
     Console.Write("Первое число не является квадратом второго");
 }
+Console.WriteLine();
