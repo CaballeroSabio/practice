e226f0e2f408f2ec03ba931ce16864fe6b506f55
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-12 09:08:21 +0300

    corrected the code

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,20 +1,21 @@
-﻿Console.Write("Write a number N: ");
-int n = int.Parse(Console.ReadLine());
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
 
+Console.Clear();
+Console.Write("Write a number N: ");
+int n = int.Parse(Console.ReadLine()); //Convert.ToInt32 - более универсальный
 int i = 0;
-
 if (n < 0)
 {
-     i = n;
+    i = n;
     n = -n;
 }
 else
 {
    i = -n;
 }
-
 while (i <= n)
 {
-    Console.Write($"{i}");
+    Console.Write($"{i} ");
     i++;
 }
+Console.WriteLine();
