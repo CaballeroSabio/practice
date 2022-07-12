ab538a5743204c46f1c1459f463290efe4caf0af
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-12 13:02:31 +0300

    corrected cod

--- Task_7/Program.cs
+++ Task_7/Program.cs
@@ -1,8 +1,18 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
 
 Console.Clear();
-Console.Write("Hello, User! Enter a three-digit number: ");
+Console.WriteLine("Hello, User!");
+Console.Write("Enter a three-digit number: ");
 int num = Convert.ToInt32(Console.ReadLine()); // при делении 456 на 10 = 45.6
-int lastDigit = num % 10;
-Console.WriteLine(lastDigit);
+
+if (num >= 100 && num < 1000)
+{
+    int lastDigit = num % 10;
+    Console.WriteLine(lastDigit);
+}
+else
+{
+    Console.WriteLine("wrong number");
+}
+Console.WriteLine("Goodbye!");
 Console.WriteLine();
