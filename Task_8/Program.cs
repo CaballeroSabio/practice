5a854f633de446136c57e109f496b2b882437116
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-12 18:54:51 +0300

    added Task_8 and created new code

---
+++ Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.Write("Write a number N: ");
+int a = int.Parse(Console.ReadLine());
+int i = 2;
+while (i <= a) 
+{ 
+    if (i % 2 == 0) 
+    { 
+    	Console.Write($"{i} "); 
+    } 
+    i++; 
+}
+Console.WriteLine();
