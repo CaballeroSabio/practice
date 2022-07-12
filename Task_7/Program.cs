640c8a61479f91245305acb867933243fc118fc6
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-12 09:19:37 +0300

    corrected the code

--- Task_7/Program.cs
+++ Task_7/Program.cs
@@ -1,5 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int num = 4567789   ; // 45.6
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.Clear();
+Console.Write("Hello, User! Enter a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine()); // при делении 456 на 10 = 45.6
 int lastDigit = num % 10;
 Console.WriteLine(lastDigit);
+Console.WriteLine();
